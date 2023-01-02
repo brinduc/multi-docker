923759d7bd3bca2df2de519f5112e8fd43168c4a
Laura Squire <brinduc@gmail.com>
2023-01-02 11:47:06 -0800

    Small title change

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React with Docker and AWS
+            Learn React with Docker on AWS
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
